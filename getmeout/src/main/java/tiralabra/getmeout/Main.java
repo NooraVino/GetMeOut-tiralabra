b4e773820c37407ffe2573dd0e987533a12bed8f
Noora Virolainen <vino@cubbli18-gold.local>
2019-10-24 21:31:23 +0300

    getmeout.jar

--- getmeout/src/main/java/tiralabra/getmeout/Main.java
+++ getmeout/src/main/java/tiralabra/getmeout/Main.java
@@ -20,7 +20,7 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        File tiedosto = new File("testi512x512hankala.txt"); //testi6x6, testi5x34, testi22x512, testi512x224, testi512x512
+        File tiedosto = new File("testi512x512hankala.txt"); //testi6x6, testi5x34, testi22x512, testi512x224, testi512x512, testi512x512hankala
         Ruudukko ruudukko = new Ruudukko();
         ruudukko.alustaTiedostosta(tiedosto);
 
